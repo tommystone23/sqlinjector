d035119f0a7cd10cbe245c46f31b4bf4e05689e4
Thomas Stone <tommy.stone23@outlook.com>
2025-06-14 10:35:34 -0400

    initial boilerplate setup and basic html handling. - use Jinja for dynamic html - return some html for index page

---
+++ sqlinjector/main.py
@@ -0,0 +1,74 @@
+import grpc
+from concurrent import futures
+import time
+import sys
+
+from grpc_health.v1.health import HealthServicer
+from grpc_health.v1.health_pb2 import HealthCheckResponse
+from grpc_health.v1 import health_pb2_grpc
+
+from proto import module_pb2
+from proto import module_pb2_grpc
+
+from sqlinjector.request_handler import RequestHandler
+from sqlinjector.request_handler import Handler
+
+class SQLInjectionServicer(module_pb2_grpc.ModuleServicer):
+    def __init__(self):
+        self.req_handler = RequestHandler()
+
+    def Register(self, request, context):
+        # store this here for now until we define a use-case for it
+        self.db_fd = "unix://" + request.store_server_address
+        response = module_pb2.RegisterResponse(
+            id="github.com/Penetration-Testing-Toolkit/sqlinjector",
+            name="SQLInjector",
+            version="0.1.0",
+        )
+
+        root_path = '/plugin/' + response.id
+        self.req_handler.set_root_path(root_path)
+
+        for key,value in self.req_handler.handlers.items():
+            route = response.routes.add()
+            route.method = value.method
+            route.path = value.path
+            route.use_sse = value.use_sse
+        
+        return response
+    
+    def Handle(self, request, context):
+        return self.req_handler.handle_request(request, context)
+    
+    def HandleSSE(self, request, context):
+        return self.req_handler.handle_request(request, context)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    module_pb2_grpc.add_ModuleServicer_to_server(SQLInjectionServicer(), server)
+
+    health = HealthServicer()
+    health.set("plugin", HealthCheckResponse.ServingStatus.Value('SERVING'))
+    health_pb2_grpc.add_HealthServicer_to_server(health, server)
+
+    # Have gRPC assign us an available port
+    address = '127.0.0.1:0'
+    port = server.add_insecure_port(address)
+    server.start()
+
+    # Add the port gRPC assigned to us
+    address = address.rsplit(':', 1)[0] + f':{port}'
+    print(f"1|1|tcp|{address}|grpc")
+    sys.stdout.flush()
+
+    try:
+        while True:
+            time.sleep(60)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+if __name__ == '__main__':
+    try:
+        serve()
+    except Exception as e:
+        print("Call to 'serve()' was unsuccessful", e)
