cc777f0e2283ac013424ad9608ef838f8b83442e
Thomas Stone <tommy.stone23@outlook.com>
2025-07-05 12:56:47 -0400

    request_handler: add start_scan function to handle requests for scan logs. - properly parse GET request parameters.

--- sqlinjector/request_handler.py
+++ sqlinjector/request_handler.py
@@ -11,12 +11,15 @@ from proto import module_pb2
 import os
 import sys
 from sqlinjector.api_handler import APIHandler
+from sqlinjector.logger_config import logger
+from urllib.parse import urlparse, parse_qs
+
 class RequestHandler:
     def __init__(self):
         self.handlers = {
             "/index"        : Handler("GET", "/index", False, self.index),
             "/data"         : Handler("GET", "/data", True, data),
-            "/request-scan" : Handler("POST", "/start-scan", True, start_scan)
+            "/start-scan"   : Handler("GET", "/start-scan", True, self.start_scan)
         }
         
         if getattr(sys, 'frozen', False):
@@ -58,6 +61,55 @@ class RequestHandler:
             body=self.template.render()
         )
 
+    def start_scan(self, request, context):
+        logger.info("starting start_scan()")
+        logger.info(f"Using url: {request.url}")
+        try:
+            url = parse_args(request.url).get('target')
+        except Exception as e:
+            logger.info(f"failed to get url: {e}")
+            return
+        task_id = self.api_handler.new_task()
+        if(not self.api_handler.start_task(task_id, url=url)):
+            logger.error(f"started task with task id: {task_id}")
+            return # log and return for now. Return some error html in the future
+        while context.is_active():
+            response_body = ''
+            while self.api_handler.task_status(task_id) != 'terminated':
+                time.sleep(5)
+            log_json = self.api_handler.task_log(task_id)
+            for log in log_json:
+                response_body += f"{log.get('message', '')}\n"
+                logger.info(log.get('message', '') + '\n')
+            response_body += "\n"
+            response = module_pb2.Response(
+                status=200,
+                header=module_pb2.Header(header={
+                    "Content-Type": module_pb2.Header.Value(values=["text/event-stream"]),
+                    "Cache-Control": module_pb2.Header.Value(values=["no-cache"]),
+                    "Connection": module_pb2.Header.Value(values=["keep-alive"])
+                }),
+                body = response_body
+            )
+            yield response
+
+            yield module_pb2.Response(
+                status=200,
+                header=module_pb2.Header(header={
+                    "Content-Type": module_pb2.Header.Value(values=["text/event-stream"]),
+                }),
+                body="STREAM ENDED"
+            )
+            return
+
+def parse_args(url: str) -> dict:
+    parsed_url = urlparse(url)
+    query_dict = parse_qs(parsed_url.query)
+
+    # Flatten the values if needed (parse_qs gives a list of values for each key)
+    return {k: v[0] if len(v) == 1 else v for k, v in query_dict.items()}
+
+# currently unused
 import time
 def data(request, context):
     count = 1
@@ -78,19 +130,3 @@ def data(request, context):
         count += 1
         if count > 5:
             break
-
-# NOTE: remember to change html JS to use Alpine stuff
-import json
-def start_scan(request, context):
-    while context.is_active():
-        response_json = {}
-        response = module_pb2.Response(
-            status=200,
-            header=module_pb2.Header(header={
-                "Content-Type": module_pb2.Header.Value(values=["text/event-stream"]),
-                "Cache-Control": module_pb2.Header.Value(values=["no-cache"]),
-                "Connection": module_pb2.Header.Value(values=["keep-alive"])
-            }),
-            body=json.dumps(response_json)
-        )
-        yield response
